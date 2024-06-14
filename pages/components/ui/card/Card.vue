93f4e58e0a24ffd4b301a04bddcbe10bc6a208ed
Sitt Guruvanich <aekazitt@gmail.com>
2024-06-14 14:05:43 +0700

    Add prettier and define prettier configs

--- pages/components/ui/card/Card.vue
+++ pages/components/ui/card/Card.vue
@@ -12,7 +12,7 @@ const props = defineProps<{
     :class="
       cn(
         'rounded-lg border border-stone-200 bg-white text-stone-950 shadow-sm dark:border-stone-800 dark:bg-stone-950 dark:text-stone-50',
-        props.class,
+        props.class
       )
     "
   >
