d24b72b8ff8afbc8ad1de7bbb5b4b9365fd1741e
Sitt Guruvanich <aekazitt@gmail.com>
2024-06-17 13:14:42 +0700

    Implement unlock and rearrange front-end cards

--- pages/App.vue
+++ pages/App.vue
@@ -4,7 +4,11 @@ import { Ref, onMounted, ref } from 'vue'
 
 /* components */
 import { Button } from '@/components/ui/button'
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
+
+/* vectors */
+import AlbyLogo from '@/assets/alby.svg'
+import BitcoinDocs from '@/assets/bitcoindocs.svg'
 import GithubBadge from '@/assets/github.svg'
 
 let albyAddress: Ref<string> = ref('')
@@ -13,6 +17,8 @@ let albyPublicKey: Ref<string> = ref('')
 let balance: Ref<number> = ref(0)
 let blockHashes: Ref<Array<string>> = ref([])
 let lockAddress: Ref<string> = ref('')
+let lockTxid: Ref<string> = ref('')
+let unlockTxid: Ref<string> = ref('')
 
 let connectWallet = async () => {
   const _window = window as typeof window & { webbtc?: any }
@@ -23,6 +29,14 @@ let connectWallet = async () => {
     albyDerivationPath.value = derivationPath
     albyPublicKey.value = publicKey
   }
+}
+
+let openBitcoinDocs = () => {
+  window.open(
+    'https://bitcoindocs.org/notes/taproot-lock-and-spend-taproot-script-path-with-python',
+    '_blank',
+    'noreferrer, noopener'
+  )
 }
 
 let openGithubRepository = () => {
@@ -65,8 +79,8 @@ let sendToLock = async () => {
     .catch(console.error)
     .then(async resp => {
       if (!!resp) {
-        let data = await resp.json()
-        console.log(data)
+        let { txid } = await resp.json()
+        lockTxid.value = txid
       }
     })
 }
@@ -79,6 +93,23 @@ let tapFaucet = async () => {
   })
     .catch(console.error)
     .then(console.log)
+}
+
+let unlock = async () => {
+  await fetch('/unlock', {
+    body: JSON.stringify({
+      address: albyAddress.value,
+      pubkey: albyPublicKey.value,
+      txid: lockTxid.value,
+    }),
+    headers: { 'Content-Type': 'application/json;' },
+    method: 'POST',
+  })
+    .catch(console.error)
+    .then(async resp => {
+      let { txid } = await resp.json()
+      unlockTxid.value = txid
+    })
 }
 
 onMounted(async () => {
@@ -99,6 +130,15 @@ onMounted(async () => {
     <div class="flex-1 space-y-4 p-8 pt-6">
       <div class="flex items-center justify-between space-y-2">
         <h2 class="text-3xl font-bold tracking-tight">Kogai</h2>
+        <Button @click="connectWallet" class="cursor-pointer float-right md:w-1/4 w-1/2" v-if="!albyAddress">
+          Connect Wallet
+          <alby-logo class="h-6 inline ml-2 w-auto" />
+        </Button>
+        <span class="float-right" v-if="albyAddress">
+          Balance:&nbsp;
+          {{ balance }}
+          &nbsp;₿
+        </span>
       </div>
     </div>
     <section class="container grid lg:grid-cols-2 place-items-center py-20 md:py-32 gap-10">
@@ -123,49 +163,116 @@ onMounted(async () => {
           </h2>
         </main>
         <div class="space-y-4 md:space-y-0 md:space-x-4">
-          <Button @click="connectWallet" class="w-full md:w-1/3" v-if="!albyAddress">
-            Connect Wallet
-          </Button>
-          <Button @click="openGithubRepository" class="md:w-1/3 w-full" variant="outline">
+          <Button
+            @click="openGithubRepository"
+            class="cursor-pointer md:w-1/3 w-full"
+            variant="outline"
+          >
             Repository
             <github-badge class="h-6 inline ml-2 w-auto" />
+          </Button>
+          <Button @click="openBitcoinDocs" class="cursor-pointer md:w-1/3 w-full" variant="outline">
+            Note by Katsu
+            <bitcoin-docs class="h-6 inline ml-2 w-auto" />
           </Button>
         </div>
       </div>
       <div class="grid gap-4 py-4 grid-cols-1">
         <Card v-if="albyAddress">
-          <CardHeader class="flex flex-row items-center justify-between space-y-0 pb-2">
-            <CardTitle class="text-sm font-medium"> Address </CardTitle>
+          <CardHeader>
+            <CardTitle> Address </CardTitle>
+            <CardDescription>
+              This is your Bitcoin Address currently selected and provided by Alby Wallet Extension.
+            </CardDescription>
           </CardHeader>
-          <CardContent>
-            <span>
-              {{ albyAddress }}
-            </span>
-            <h3>Balance</h3>
-            <span>
-              {{ balance }}
-            </span>
-            <Button @click="createTaprootAddress" class="float-right mb-4 mt-4">
-              <span class="cursor-pointer"> Create Taproot Address </span>
-            </Button>
-            <Button @click="tapFaucet" class="float-right mb-4 mt-4" variant="secondary">
-              <span class="cursor-pointer"> Tap Faucet </span>
-            </Button>
-            <Button @click="fetchBalance" class="float-right mb-4 mt-4" variant="outline">
-              <span class="cursor-pointer"> Fetch Balance </span>
-            </Button>
+          <CardContent class="break-all text-sm font-medium">
+            {{ albyAddress }}
           </CardContent>
+          <CardFooter class="justify-between space-x-2">
+            <Button
+              @click="tapFaucet"
+              class="cursor-pointer"
+              variant="secondary"
+            >
+              Tap Faucet
+            </Button>
+            <Button
+              @click="fetchBalance"
+              class="cursor-pointer"
+              variant="outline"
+            >
+              Fetch Balance
+            </Button>
+          </CardFooter>
+        </Card>
+        <Card v-if="albyAddress && !lockAddress">
+          <CardHeader>
+            <CardTitle> Create Taproot Lock </CardTitle>
+            <CardDescription>
+              Here we will construct an address that locks its funds that will only be spendable if
+              any of the 3 mathematical equations get solved.
+            </CardDescription>
+          </CardHeader>
+          <CardFooter class="flex flex-col items-end">
+            <Button @click="createTaprootAddress" class="cursor-pointer">
+              Create Taproot Address
+            </Button>
+          </CardFooter>
         </Card>
         <Card v-if="lockAddress">
-          <CardHeader class="flex flex-row items-center justify-between space-y-0 pb-2">
-            <CardTitle class="text-sm font-medium"> Lock Address </CardTitle>
+          <CardHeader>
+            <CardTitle> Lock Address </CardTitle>
+            <CardDescription>
+              After getting an address, send 0.01 BTC to it. In the next step, we'll solve one of
+              the equations and spend those funds.
+            </CardDescription>
           </CardHeader>
-          <CardContent>
+          <CardContent class="break-all text-sm font-medium">
             {{ lockAddress }}
           </CardContent>
-          <Button @click="sendToLock" class="float-right mb-4 mt-4" variant="outline">
-            <span class="cursor-pointer"> Send to Lock Address </span>
-          </Button>
+          <CardFooter class="flex flex-col items-end">
+            <Button
+              @click="sendToLock"
+              :variant="!lockTxid ? '' : 'outline'"
+              class="cursor-pointer"
+            >
+              Send 0.1 BTC to Lock Address
+            </Button>
+          </CardFooter>
+        </Card>
+        <Card v-if="lockTxid">
+          <CardHeader>
+            <CardTitle> Locking Transaction Hash </CardTitle>
+            <CardDescription>
+              In this example, we will unlock the funds by solving the 2nd mathematical equation
+              (x + 2 = 4). In order to unlock the funds we must provide a value X.
+            </CardDescription>
+          </CardHeader>
+          <CardContent class="break-all font-medium text-sm">
+            {{ lockTxid }}
+          </CardContent>
+          <CardFooter class="flex flex-col items-end">
+            <Button @click="unlock" class="cursor-pointer float-right mb-4 mt-4" :variant="!unlockTxid ? '' : 'outline'">
+              Unlock
+            </Button>
+          </CardFooter>
+        </Card>
+        <Card v-if="unlockTxid">
+          <CardHeader>
+            <CardTitle> Unlocking Transaction Hash </CardTitle>
+          </CardHeader>
+          <CardContent class="break-all text-sm font-medium">
+            {{ unlockTxid }}
+          </CardContent>
+          <CardFooter class="flex flex-col items-end">
+            <Button
+              @click="fetchBalance"
+              class="cursor-pointer float-right mb-4 mt-4"
+              variant="secondary"
+            >
+              Fetch unlocked balance
+            </Button>
+          </CardFooter>
         </Card>
       </div>
       <div class="grid gap-4 py-4 grid-cols-1 hidden">
--- serve.py
+++ serve.py
@@ -2,8 +2,19 @@
 from asyncio import sleep
 from binascii import unhexlify
 from bitcointx import select_chain_params
+from bitcointx.core import (
+  CMutableTransaction,
+  COutPoint,
+  CTxIn,
+  CTxOut,
+  CTxInWitness,
+  CTxWitness,
+  b2x,
+  lx,
+)
 from bitcointx.core.script import (
   CScript,
+  CScriptWitness,
   OP_ADD,
   OP_1,
   OP_2,
@@ -13,24 +24,26 @@ from bitcointx.core.script import (
   TaprootScriptTree,
 )
 from bitcointx.rpc import RPCCaller
-from bitcointx.wallet import P2TRCoinAddress
+from bitcointx.wallet import P2TRBitcoinRegtestAddress, P2TRCoinAddress
 from bitcointx.wallet import CCoinKey
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
+from fastapi.param_functions import Depends
 from fastapi.requests import Request
 from fastapi.responses import HTMLResponse
 from json import dumps
 from pydantic import BaseModel, StrictStr
 from sse_starlette.sse import EventSourceResponse
 from rizzler import Rizzler, RizzleTemplates
-from typing import AsyncGenerator, Dict, Literal, Union
+from typing import Annotated, AsyncGenerator, Dict, Literal, Union
 from uuid import uuid4 as uuid
 
+
+select_chain_params("bitcoin/regtest")
 
 @asynccontextmanager
 async def lifespan(_: FastAPI):
   await Rizzler.serve()
-  select_chain_params("bitcoin/regtest")
   caller: RPCCaller = RPCCaller("http://aesir:aesir@localhost", 18443)
   caller.connect()
   if len(caller.listwallets()) == 0:
@@ -39,6 +52,21 @@ async def lifespan(_: FastAPI):
     caller.generatetoaddress(100, treasury)
   yield
   Rizzler.shutdown()
+
+
+def taproot_script_tree() -> TaprootScriptTree:
+  ### define the 3 spending equations ###
+  script_a: bytes = CScript([OP_1, OP_ADD, OP_2, OP_EQUAL], name="a")
+  script_b: bytes = CScript([OP_2, OP_ADD, OP_4, OP_EQUAL], name="b")
+  script_c: bytes = CScript([OP_1, OP_ADD, OP_5, OP_EQUAL], name="c")
+  ### bitcointx will build a tree of branches and leaves for you ###
+  return TaprootScriptTree([script_a, script_b, script_c])
+
+
+def rpc_caller() -> RPCCaller:
+  caller: RPCCaller = RPCCaller("http://aesir:aesir@localhost", 18443)
+  caller.connect()
+  return caller
 
 
 app = FastAPI(lifespan=lifespan)
@@ -50,10 +78,9 @@ def index(request: Request) -> HTMLResponse:
   return templates.TemplateResponse("index.html", {"request": request})
 
 
-async def blocks_generator() -> (
-  AsyncGenerator[Dict[Literal["event", "id", "data"], Union[int, str]], None]
-):
-  caller: RPCCaller = RPCCaller("http://aesir:aesir@localhost", 18443)
+async def blocks_generator(
+  caller: RPCCaller,
+) -> AsyncGenerator[Dict[Literal["event", "id", "data"], Union[int, str]], None]:
   caller.connect()
   yield {
     "event": "message",
@@ -64,17 +91,17 @@ async def blocks_generator() -> (
 
 
 @app.get("/balance/{address}")
-async def fetch_balance(address: str) -> Dict[Literal["balance"], float]:
-  caller: RPCCaller = RPCCaller("http://aesir:aesir@localhost", 18443)
-  caller.connect()
-  txouts: list = caller.scantxoutset("start", [{"desc": f"addr({ address })"}])
+async def fetch_balance(
+  address: str, caller: Annotated[RPCCaller, Depends(rpc_caller)]
+) -> Dict[Literal["balance"], float]:
+  txouts: dict = caller.scantxoutset("start", [{"desc": f"addr({ address })"}])
   caller.scantxoutset("abort", [{"desc": f"addr({ address })"}])
   return {"balance": txouts.get("total_amount", 0.0)}
 
 
 @app.get("/blocks")
-def fetch_blocks() -> EventSourceResponse:
-  return EventSourceResponse(blocks_generator())
+def fetch_blocks(caller: Annotated[RPCCaller, Depends(rpc_caller)]) -> EventSourceResponse:
+  return EventSourceResponse(blocks_generator(caller))
 
 
 class FaucetPayload(BaseModel):
@@ -82,10 +109,7 @@ class FaucetPayload(BaseModel):
 
 
 @app.post("/faucet")
-async def faucet(payload: FaucetPayload) -> str:
-  caller: RPCCaller = RPCCaller("http://aesir:aesir@localhost", 18443)
-  caller.connect()
-  ### Generate to core and send to targeted addresss ###
+async def faucet(caller: Annotated[RPCCaller, Depends(rpc_caller)], payload: FaucetPayload) -> str:
   caller.generatetoaddress(6, payload.address)
   return "OK"
 
@@ -95,15 +119,9 @@ class LockPayload(BaseModel):
 
 
 @app.post("/lock")
-async def lock(payload: LockPayload) -> Dict[Literal["address"], str]:
-  # define the 3 spending equations
-  script_a: bytes = CScript([OP_1, OP_ADD, OP_2, OP_EQUAL], name="a")
-  script_b: bytes = CScript([OP_2, OP_ADD, OP_4, OP_EQUAL], name="b")
-  script_c: bytes = CScript([OP_1, OP_ADD, OP_5, OP_EQUAL], name="c")
-  # bitcointx will build a tree of branches and leaves for you
-  tree: TaprootScriptTree = TaprootScriptTree([script_a, script_b, script_c])
-  # create a provably unspendable public key in order to
-  # make the coins spendable ONLY using the script path
+async def lock(payload: LockPayload, tree: Annotated[TaprootScriptTree, Depends(taproot_script_tree)]) -> Dict[Literal["address"], str]:
+  ### create a provably unspendable public key in order to ###
+  ### make the coins spendable ONLY using the script path ###
   internal_pubkey = CCoinKey.from_secret_bytes(unhexlify(payload.pubkey[2:])).xonly_pub
   tree.set_internal_pubkey(internal_pubkey)
   address: P2TRCoinAddress = P2TRCoinAddress.from_script_tree(tree)
@@ -116,20 +134,50 @@ class SendPayload(BaseModel):
 
 
 @app.post("/send")
-async def send_payment(payload: SendPayload) -> str:
-  caller: RPCCaller = RPCCaller("http://aesir:aesir@localhost", 18443)
-  caller.connect()
-  caller.sendtoaddress(payload.address, payload.amount)
-  return "OK"
+async def send_payment(
+  caller: Annotated[RPCCaller, Depends(rpc_caller)], payload: SendPayload
+) -> Dict[Literal["txid"], str]:
+  txid: str = caller.sendtoaddress(payload.address, payload.amount)
+  return {"txid": txid}
 
 
 class UnlockPayload(BaseModel):
   address: StrictStr
+  pubkey: StrictStr
+  txid: StrictStr
 
 
 @app.post("/unlock")
-async def send_unlock_payment(payload: UnlockPayload) -> str:
-  return "OK"
+async def send_unlock_payment(
+  caller: Annotated[RPCCaller, Depends(rpc_caller)],
+  tree : Annotated[TaprootScriptTree, Depends(taproot_script_tree)],
+  payload: UnlockPayload,
+) -> Dict[Literal["txid"], str]:
+  internal_pubkey = CCoinKey.from_secret_bytes(unhexlify(payload.pubkey[2:])).xonly_pub
+  tree.set_internal_pubkey(internal_pubkey)
+  ### set the new destination address ###
+  destination_address = P2TRBitcoinRegtestAddress(payload.address)
+
+  ### construct a spending transaction ###
+  script, control_block = tree.get_script_with_control_block("b")  # type: ignore
+
+  ### get the txid and index of the transaction that funded the ###
+  ### address which we generated above ###
+  txin = CTxIn(COutPoint(lx(payload.txid), 1))
+  txout = CTxOut(998000, destination_address.to_scriptPubKey())
+  tx = CMutableTransaction([txin], [txout])
+
+  ### we pass the encoded num 2 (x + 2 = 4; x = 4 - 2; x = 2) which is the answer ###
+  ### to script b that we are using to unlock the funds ###
+  ctxinwitnesses = [CTxInWitness(CScriptWitness([2, script, control_block]))]
+  tx.wit = CTxWitness(ctxinwitnesses)  # type: ignore
+
+  ### Broadcast ###
+  try:
+    return {"txid": caller.sendrawtransaction(b2x(tx.serialize()), 0)}
+  except Exception as err:
+    print(err)
+    return {"txid": ""}
 
 
 if __name__ == "__main__":
