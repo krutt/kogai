9b00dee65064fdeff5b1d1f67695df8f86ea3016
Sitt Guruvanich <aekazitt@gmail.com>
2024-06-18 20:56:26 +0700

    Remove unused accordion keyframes

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,6 @@ module.exports = {
   darkMode: ["class"],
   content: [
     './pages/**/*.{js,jsx,vue}',
-    './components/**/*.{js,jsx,vue}',
-    './app/**/*.{js,jsx,vue}',
-    './src/**/*.{js,jsx,vue}',
 	],
   prefix: "",
   theme: {
@@ -16,22 +13,6 @@ module.exports = {
       padding: "2rem",
       screens: {
         "2xl": "1400px",
-      },
-    },
-    extend: {
-      keyframes: {
-        "accordion-down": {
-          from: { height: 0 },
-          to: { height: "var(--radix-accordion-content-height)" },
-        },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: 0 },
-        },
-      },
-      animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
   },
